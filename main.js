44882c9cd4152394a607460776cb705f1d3c69b5
Vikalp Malhotra <kimboho@gmail.com>
2021-12-21 14:07:59 -0500

    commit tree 04aa25c7b5ffb10bcad90edfb5219cc4970d8b12

--- main.js
+++ main.js
@@ -5,15 +5,54 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+function init() {
+
+  let errorMess = document.getElementById('modal');
+  // errorMess.hidden = true;
+
+  let like = document.getElementsByClassName('like');
+  console.log(like);
+
+  mimicServerCall(url = "http://mimicServer.example.com", config = {})
+    .then(res => {
+      console.log('its a success')
+      let actHeart = document.getElementsByClassName('like-glyph')
+
+      for (let i = 0; i < actHeart.length; i++) {
+        actHeart[i].addEventListener("click", fill => {
+          if (actHeart[i].innerHTML == EMPTY_HEART) {
+            actHeart[i].innerHTML = FULL_HEART
+            actHeart[i].classList.add("activated-heart")
+
+          } else {
+            actHeart[i].innerHTML = EMPTY_HEART;
+            actHeart[i].classList.remove("activated-heart")
+
+          }
+        })
+      }
+      console.log(actHeart);
+
+    })
+    .catch(data => {
+      errorMess.classList.remove("hidden");
+      setTimeout(() => errorMess.classList.add("hidden")
+        , 3000)
+
+    })
+
+}
+
+init()
 
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
@@ -23,3 +62,6 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+
+
